bb5ba531a33548cbe03956207681b40f9ab75165
Ferran <ferranhendriks@hotmail.com>
2022-06-17 22:01:58 +0200

    Initial commit

---
+++ src/test/java/ga/fhendriks/videoShare/VideoShareApplicationTests.java
@@ -0,0 +1,13 @@
+package ga.fhendriks.videoShare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoShareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
